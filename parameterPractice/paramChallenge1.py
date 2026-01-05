0aaf3d380c2edd1b2e9a7b82a3157ba5242dc53c
akiedes <akiedes@vtsd.com>
2026-01-05 12:28:43 -0500

    1.2.3 Update

--- parameterPractice/paramChallenge1.py
+++ parameterPractice/paramChallenge1.py
@@ -1,10 +1,21 @@
 # Make 3 methods following the details given here.
+
 # Method 1: name it "tic" and it takes 2 parameters "num1" and "num2" and returns
 # the value of the 2 parameters when subtracted (ie: num1 - num2)
+def tic(num1, num2):
+    return num1 - num2
 
 # Method 2: name it "tac" and it takes 1 parameter "exp"
 # use a loop to multiply the number 5 by itself "exp" times
 # return that value
+def tac(exp):
+    sum = 1
+
+    for num in range(exp):
+        sum = sum * 5
+
+    return sum
+
 
 # Method 3: name it "toe" that takes no parameters.  This method should print
 # the results of a method call to "tic(3, 5)" and "tac(4)" to the console
